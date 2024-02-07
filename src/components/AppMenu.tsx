1c7e0058d50037230112b593b6c0a4c871d24e74
Ahmed Bilal <ahmed.bilal@t-online.de>
2024-02-07 21:08:27 +0100

    added needed files to branch

---
+++ src/components/AppMenu.tsx
@@ -0,0 +1,15 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+const AppMenu = () => {
+  return <div>AppMenu</div>;
+};
+
+export default AppMenu;
